7984ce565d4e0a519b3b35f366be279d6537280a
Dayapal <126281347+Dayapal@users.noreply.github.com>
2025-07-12 12:45:54 +0530

    Update tweet.model.js
    did all the changes in tweet models

--- src/models/tweet.model.js
+++ src/models/tweet.model.js
@@ -4,10 +4,12 @@ const tweetSchema = new Schema({
     content: {
         type: String,
         required: true
+        
     },
     owner: {
         type: Schema.Types.ObjectId,
         ref: "User"
+        
     }
 }, {timestamps: true})
 
